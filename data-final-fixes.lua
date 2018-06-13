37f327710284bd603ac44c0b372f2fc95e2a60c7
TheBrain0110 <tonofpudding@gmail.com>
2018-06-13 01:54:40 -0400

    Add if check for other mods' items

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,6 +1,8 @@
 if deadlock_stacking then
 
     deadlock_stacking.create("concrete", nil, "deadlock-stacking-2")
-    deadlock_stacking.create("SiSi-silicon", nil, "deadlock-stacking-2")
-    deadlock_stacking.create("SiSi-silicon-wafer", nil, "deadlock-stacking-2")
+    if mods["SimpleSilicon"] then
+        deadlock_stacking.create("SiSi-silicon", nil, "deadlock-stacking-2")
+        deadlock_stacking.create("SiSi-silicon-wafer", nil, "deadlock-stacking-2")
+    end
 end
