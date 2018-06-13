fa42888ffa2082003af4d6b229d9648fbde6055e
TheBrain0110 <tonofpudding@gmail.com>
2018-06-13 01:53:58 -0400

    Remove control.lua
    Unnecessary for this simple addon

--- control.lua
+++
@@ -1,7 +0,0 @@
-script.on_init(function()
-for index, force in pairs(game.forces) do
-    local technologies = force.technologies
-    local recipes = force.recipes
-
-    technologies["deadlock-stacking-2"].reload()
-)
